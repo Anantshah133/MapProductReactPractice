196809927458a038910978bb12a12ccd04d39d79
Anantshah133 <anantshah130305@gmail.com>
2023-08-29 18:57:38 +0530

    Just The begining of card practice.

---
+++ src/components/Product-Area.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+function ProductArea() {
+    return (
+        <div class="card" style={{width: '18rem'}}>
+            <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="index.html" class="btn btn-primary">Go somewhere</a>
+                </div>
+        </div>
+    )
+}
+export default ProductArea;
