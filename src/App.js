81ba9b8b3a9524352276b6b2e5513dad7616bc65
Jay Doshi <jaydoshi.iosdeveloper@gmail.com>
2023-09-12 12:16:02 +0530

    Added Filter and Search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header';
 import ProductArea from './components/Product-Area';
+import ProductList from './components/ProductList';
+import Data from '../src/components/product-content';
 
 function App() {
   return (
     <>
       <Header />
-      <ProductArea />
+      {/* <ProductArea /> */}
+      <ProductList Products={Data}/>
     </>
   )
 }
--- src/components/product-content.jsx
+++ src/components/product-content.jsx
@@ -1,51 +1,64 @@
 const Products = [
+
     {
         image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage2/product1.png",
-        name: "Lace Shirt Cut ",
+        name: "Shirt",
         description: "Please Buy This Product",
         price: 100,
     },
     {
         image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage2/product2.png",
-        name: "Lace Shirt Cut ",
+        name: "Pant",
         description: "Please Buy This Product",
         price: 200,
     },
     {
         image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage2/product3.png",
-        name: "Lace Shirt Cut ",
+        name: "Jeans",
         description: "Please Buy This Product",
         price: 300,
     },
     {
         image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage2/product4.png",
-        name: "Lace Shirt Cut ",
+        name: "Blazer",
         description: "Please Buy This Product",
         price: 400,
     },
     {
         image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage2/product5.png",
-        name: "Lace Shirt Cut ",
+        name: "Suit",
         description: "Please Buy This Product",
         price: 500,
     },
     {
         image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage2/product6.png",
-        name: "Lace Shirt Cut ",
+        name: "Shirt",
         description: "Please Buy This Product",
         price: 600,
     },
     {
         image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage2/product7.png",
-        name: "Lace Shirt Cut ",
+        name: "Pant",
         description: "Please Buy This Product",
         price: 700,
     },
     {
         image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage2/product8.png",
-        name: "Lace Shirt Cut ",
+        name: "Jeans",
         description: "Please Buy This Product",
         price: 800,
+    },
+    {
+        image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage1/product1-hover.png",
+        name: "Jeans",
+        description: "Please Buy This Product",
+        price: 900,
+    },
+    {
+        image: "https://wp.alithemes.com/html/guza/demos/assets/imgs/page/homepage1/product2-hover.png",
+        name: "Blazer",
+        description: "Please Buy This Product",
+        price: 1000,
     },
 ];
 
